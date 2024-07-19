4b9c10a813258512cd7e1c668797b984f5d42195
Ziyuan Wang <53027037+ZyuanW@users.noreply.github.com>
2024-07-19 09:40:28 +0800

    Update SIBR_viewer.py

--- SIBR_viewer.py
+++ SIBR_viewer.py
@@ -1,9 +1,9 @@
 import subprocess
 
-# output保存路径
-model_path = r'C:\Users\ziyuanw\Desktop\gaussian-splatting\gaussian-splatting\output\ggbond'
+# output_path
+model_path = r'C:\Users\ziyuanw\Desktop\gaussian-splatting\output'
 
-# 脚本执行
+# start the viewer
 command = f'SIBR_gaussianViewer_app.exe -m {model_path}'
 run_path = 'external/viewers/bin'
 subprocess.run(command, shell=True, cwd=run_path)
